d720c887a5988f0db8899c2c3752dd7ff34dffda
AYRtonMeD <ayrtondantas2013@gmail.com>
2025-02-23 23:16:43 -0300

    feat: aula 3

--- src/app/invite/[subscriberId]/page.tsx
+++ src/app/invite/[subscriberId]/page.tsx
@@ -1,11 +1,19 @@
 import Image from "next/image";
-import logo from '../../assets/logo.svg';
+import logo from '../../../assets/logo.svg';
 import { Ranking } from "./ranking";
 import { Stats } from "./stats";
 import { InviteLinkInput } from "./invite-link-input";
 
-export default function InvitePage() {
-  const inviteLink = 'http://localhost:3000/invite/39dkf390234dsf42'
+interface invitePageProps {
+  params: Promise <{
+    subscriberId: string
+  }>
+}
+
+export default async function InvitePage(props: invitePageProps) {
+  const { subscriberId } = await props.params
+
+  const inviteLink = `http://localhost:3333/invites/${subscriberId}`
 
   return (
     <div className="min-h-dvh flex items-center justify-between gap-16 flex-col 
@@ -41,7 +49,7 @@ export default function InvitePage() {
         </div>
 
         <InviteLinkInput inviteLink={inviteLink} />
-        <Stats />
+        <Stats subscriberId = {subscriberId} />
       </div>
     </div>
     <Ranking />
